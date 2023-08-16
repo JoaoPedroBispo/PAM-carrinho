d0c732d11f36637745391eaf3f8da88439cf95f7
JoaoPedroBispo <jopbispo83@gmail.com>
2023-08-16 15:41:30 -0300

    Atualização de Texto

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
-import {Image} from 'react-native'
-import topo from "../../../assets/topo.png"
+import React from "react";
+import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
+import topo from "../../../assets/topo.png";
 
-export default function Carrinho(){
-    return <Image source={topo}/>
+const width = Dimensions.get("screen").width;
+export default function Carrinho() {
+  return (
+    <>
+      <Image source={topo} style={estilos.topo} />
+      <Text style={estilos.titulo}>Detelhe do Carrinho</Text>
+      <View>
+        <Text>Carrinho de compras</Text>
+        <Text>Fazenda Etec Cidade do Livro</Text>
+        <Text>
+          Uma cesta com produtos selecionados cuidadosamente da fazenda direto
+          para sua cozinha
+        </Text>
+        <Text>R$ 40,00</Text>
+      </View>
+    </>
+  );
 }
+
+const estilos = StyleSheet.create({
+  topo: {
+    width: "100%",
+    height: (578 / 768) * width,
+  },
+
+  titulo: {
+    fontSize: 24,
+    fontWeight: "bold",
+    lineHeight: 26,
+    width: "100%",
+    textAlign: "center",
+    color: "white",
+    position: "absolute",
+    padding: 16,
+  },
+});
